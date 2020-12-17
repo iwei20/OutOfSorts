195ce6aeb9f9687694d5060687e91450204200b9
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2020-12-17 12:41:38 -0500

    Implement insertionSort

--- Sorts.java
+++ Sorts.java
@@ -29,4 +29,15 @@ public class Sorts{
             }
         }
     }
+
+    public static void insertionSort(int[] data) {
+        for(int i = 1; i < data.length; ++i) {
+            int temp = data[i];
+            int j = i - 1;
+            for(; j >= 0 && data[j] > temp; --j) {
+                data[j + 1] = data[j];
+            }
+            data[j + 1] = temp;
+        }
+    }
 }
