ed49525e6f71a8a33d0697afb95cc23bded639cb
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2020-12-17 12:31:16 -0500

    Test selectionSort

--- Tester.java
+++ Tester.java
@@ -25,5 +25,17 @@ public class Tester {
         Sorts.bubbleSort(linear);
         Arrays.sort(correctlinear);
         System.out.println(Arrays.equals(linear, correctlinear));
+
+        oneToSix = new int[]{5, 3, 4, 2, 1, 6};
+        Sorts.selectionSort(oneToSix);
+        System.out.println(Arrays.toString(oneToSix));
+        
+        for(int i = 0; i < 10000; ++i) {
+            correctsquared[i] = nsquared[i] = rng.nextInt();
+        }
+        Arrays.sort(correctsquared);
+        Sorts.selectionSort(nsquared);
+        
+        System.out.println(Arrays.equals(nsquared, correctsquared));
     }
 }
